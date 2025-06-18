4f212e23618bbb8d1f9256e380840fe68552e815
Jal2406 <mayanijal@gmail.com>
2025-06-18 11:11:31 +0530

    Improvised Handeling

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const cors = require('cors');
 const app = express();
-app.use(cors({
-  origin: 'http://localhost:3001',
-  credentials: true
-}));
+// app.use(cors({
+//   origin: process.env.CLIENT_URL || 'http://localhost:3001',
+//   credentials: true
+// }));
+
 const loginRoute = require('./Routes/login')
 const signupRoute = require('./Routes/signup');
 const path = require('path');
@@ -12,7 +13,7 @@ app.use('/images', express.static(path.join(__dirname, 'public/images')));
 app.use(express.json())
 app.get('/', (req, res) => {
   res.send('API is running...');
-});
+})
 app.use('/login', loginRoute)
 app.use('/signup', signupRoute)
 app.use('/product', require('./Routes/product'))
@@ -21,6 +22,7 @@ app.use('/admin', require('./Routes/admin'));
 app.use('/payment', require('./Routes/payment'));
 
 
-app.listen(3000, () => {
-  console.log(`Server running on http://localhost:3000`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
