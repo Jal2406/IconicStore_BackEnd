04c49e096422718a44d9ba182e73f614902063a6
Jal2406 <mayanijal@gmail.com>
2025-06-18 11:06:37 +0530

    API Check

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const signupRoute = require('./Routes/signup');
 const path = require('path');
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 app.use(express.json())
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 app.use('/login', loginRoute)
 app.use('/signup', signupRoute)
 app.use('/product', require('./Routes/product'))
